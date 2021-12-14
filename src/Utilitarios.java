9b75129cd6fce4381ec79293cc35cf29896c96e4
Angello Picasso <anpicass@pichincha.com>
2021-12-14 17:37:20 -0500

    se cambian los nombres de los métodos para que sean verbos

--- src/Utilitarios.java
+++ src/Utilitarios.java
@@ -2,7 +2,7 @@ import java.util.stream.LongStream;
 
 public class Utilitarios {
 
-    public int promedio(int... params) {
+    public int calcularPromedio(int... params) {
         int avg = 0;
         for (int param : params) {
             avg = avg + param;
@@ -10,21 +10,21 @@ public class Utilitarios {
         return avg / params.length;
     }
 
-    public double factorial(int numero) {
+    public double calcularFactorial(int numero) {
         return LongStream.rangeClosed(1, numero).reduce(1, (long x, long y) -> x * y);
     }
 
-    public double areaCirculo(double r) {
+    public double calcularAreaCirculo(double r) {
         final double PI = 22.0 / 7.0;
         return PI * (r * r);
     }
 
     //b = base ; h= altura
-    public double areaTriangulo(double b, double h) {
+    public double calcularAriangulo(double b, double h) {
         return (b * h) / 2.0;
     }
 
-    public double calculoIva(double valor) {
+    public double calcularIva(double valor) {
         final int IVA = 12;
         return (valor * IVA) / 100;
     }
