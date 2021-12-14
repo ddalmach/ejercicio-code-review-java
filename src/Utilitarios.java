c6f5e1a25c95c103550d94f44f307c7441e92d73
jhonny calderon <yamit3@gmail.com>
2021-12-14 17:29:54 -0500

    Bugs y code smells

--- src/Utilitarios.java
+++ src/Utilitarios.java
@@ -2,8 +2,10 @@ public class Utilitarios {
     public int average(int params[]) {
         int avg = 0;
         for (int i = 0; i < params.length; i++) {
+            //TODO usar sobrecarga +=
             avg = avg + params[i];
         }
+        //TODO divicion por cero.
         return avg / params.length;
     }
 
@@ -13,6 +15,7 @@ public class Utilitarios {
             return resultado;
         }
         for (int i = 2; i <= numero; i++) {
+            //TODO usar sobrecarga  resltado *= i
             resultado = resultado * i;
 
         }
@@ -20,19 +23,23 @@ public class Utilitarios {
     }
 
     public double areaCirculo(double r) {
+        //TODO valor constante, separar declaracion en contate estatica
         final double PI = 22 / 7;
         return PI * (r * r);
     }
 
     //b = base ; h= altura
     public double areaTriangulo(double b, double h) {
+        //TODO vairable extra, se puede retornar directamente el resultado.
         double resultado = 0d;
         resultado = (b * h) / 2;
         return resultado;
     }
 
     public double calculoIva(double valor) {
+        //TODO valor contante, separar en variable estatica
         final int IVA = 12;
+        //TODO se recomienda formula  iva * valor, donde el valor del iva sea una variable constante = 12/100
         return (valor * IVA) / 100;
     }
 
