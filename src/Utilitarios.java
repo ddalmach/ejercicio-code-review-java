70424809364a919effb8e40c35ae9e8cec3b9440
Miguel Alexander Mejía <87379029+bp-mmejia@users.noreply.github.com>
2021-12-14 17:30:53 -0500

    Update Utilitarios.java
    Observaciones

--- src/Utilitarios.java
+++ src/Utilitarios.java
@@ -4,10 +4,12 @@ public class Utilitarios {
         for (int i = 0; i < params.length; i++) {
             avg = avg + params[i];
         }
+        //Si la lista es vacía daría una excepción, validar por favor
         return avg / params.length;
     }
 
     public double factorial(int numero) {
+        //se debería inicializar el double con un decimal
         double resultado = 1;
         if (numero == 1 || numero == 0) {
             return resultado;
@@ -19,19 +21,26 @@ public class Utilitarios {
         return resultado;
     }
 
+    //El nombre del parámetro radius o radio sería más entendible
     public double areaCirculo(double r) {
+        //De preferencia utiliza la constante de la librería Math "Math.PI"
         final double PI = 22 / 7;
+        //De preferencia podrías utilizar Math.sqrt
         return PI * (r * r);
     }
 
+    //Comentario innecesario, los parámetros deberían tener esos nombres
     //b = base ; h= altura
     public double areaTriangulo(double b, double h) {
+        //Inicialización Innecesaria
         double resultado = 0d;
+        //Se podría directamente retornar el resultado
         resultado = (b * h) / 2;
         return resultado;
     }
 
     public double calculoIva(double valor) {
+        //La constante no se utiliza en ningún otro lugar así que es innecesario que se declare como tal
         final int IVA = 12;
         return (valor * IVA) / 100;
     }
