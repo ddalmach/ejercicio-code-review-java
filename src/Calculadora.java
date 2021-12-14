0da1f34f384051563b8d29c09a335e4fe9709578
Miguel Alexander Mejía <87379029+bp-mmejia@users.noreply.github.com>
2021-12-14 17:22:44 -0500

    Update Calculadora.java
    Class comments

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,31 +3,50 @@
  */
 public class Calculadora {
 
+    // los métodos deberían ser privados
     public int suma(int num1, int num2) {
+        //comentario innecesario o en su defecto no descriptivo (una suma no se crea, se calcula)
         //metodo para crear una suma
+        
+        //no hace falta inicializar una variable
         int resul = 0;
+        //basta con retornar el resultado
         resul = num1 + num2;
         return resul;
     }
 
     public int resta(int num1, int num2) {
+        //comentario innecesario o en su defecto no descriptivo (una resta no se crea, se calcula)
         //metodo para crear la resta
+        
+        //no hace falta inicializar una variable
         int resul = 0;
+        //basta con retornar el resultado
         resul = num1 - num2;
         return resul;
     }
-
+//nombre del método (debería ser multiplicar)
     public int multipli(int num1, int num2) {
+        //comentario innecesario o en su defecto no descriptivo (una multiplicación no se crea, se calcula)
         //metodo para crear la multiplicacion
+        
+        //no hace falta inicializar una variable
         int resul = 0;
+        //basta con retornar el resultado
         resul = num1 * num2;
         return resul;
     }
-
+//nombre del método (debería ser dividir)
     public double divi(int num1, int num2) {
+        //comentario innecesario o en su defecto no descriptivo (una división no se crea, se calcula)
         //metodo para la division
+        
+        //comentario improsedente
         //double es para un valor con punto decimal
+        
+        //no hace falta inicializar una variable, además se debería inicializar con el punto decimal
         double resul = 0;
+        //basta con retornar el resultado
         resul = num1 / num2;
         return resul;
     }
