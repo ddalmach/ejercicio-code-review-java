76de9410af884811dc9ed2703d5336393fc18e6c
Angello Picasso <anpicass@pichincha.com>
2021-12-14 17:23:11 -0500

    Si se va a usar español, usar español para todo. Se nombre método average -> promedio

--- src/Utilitarios.java
+++ src/Utilitarios.java
@@ -1,5 +1,6 @@
 public class Utilitarios {
-    public int average(int params[]) {
+
+    public int promedio(int params[]) {
         int avg = 0;
         for (int i = 0; i < params.length; i++) {
             avg = avg + params[i];
